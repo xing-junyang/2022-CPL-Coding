1ea4a60814593bf9debbaeadc33634f127e21da7
Junyang Xing <1057067557@qq.com>
2022-09-23 17:12:02 +0800

    2022/9/23

---
+++ a+b_program.c
@@ -0,0 +1,11 @@
+//
+// Created by 邢骏洋 on 2022/9/23.
+//
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+    return 0;
+}
---
+++ hello,world.c
@@ -0,0 +1,9 @@
+//
+// Created by 邢骏洋 on 2022/9/23.
+//
+#include <stdio.h>
+
+int main(){
+    printf("hello,world!");
+    return 0;
+}
