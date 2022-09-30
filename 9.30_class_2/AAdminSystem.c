8e2282e6bbbeac5c190b1734bf334acf79022eb4
Junyang Xing <1057067557@qq.com>
2022-09-30 17:00:56 +0800

    Adding AAdminSystem.c

---
+++ 9.30_class_2/AAdminSystem.c
@@ -0,0 +1,20 @@
+/**************************************************
+--------------- PROGRAM INFORMATION ---------------
+ Project Name: A (Naive) Administration System
+ File Name: AAdminSystem.c
+ Author: xingjunyang
+ Email: xingjunyang@smail.nju.edu.cn
+ Date of Creation: 2022/9/30
+ Brief description of this program: ..
+----------------------- END -----------------------
+**************************************************/
+#include <stdio.h>
+
+struct Data{
+
+};
+
+int main(){
+
+    return 0;
+}
