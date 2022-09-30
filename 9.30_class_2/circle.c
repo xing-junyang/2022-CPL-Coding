76eb6b3730ceb47e7a233594fe90bd09bf6494b2
Junyang Xing <1057067557@qq.com>
2022-09-30 16:22:14 +0800

    Adding 3 files to 9.30_class_2

---
+++ 9.30_class_2/circle.c
@@ -0,0 +1,18 @@
+/**************************************************
+--------------- PROGRAM INFORMATION ---------------
+ Project Name: circle
+ File Name: circle.c
+ Author: xingjunyang
+ Email: xingjunyang@smail.nju.edu.cn
+ Date of Creation: 2022/9/30
+ Brief description of this program: To calculate c of a circle;
+----------------------- END -----------------------
+**************************************************/
+#include <stdio.h>
+
+const double PI=3.1415926535;
+
+int main(){
+    int radius = 10;
+
+}
