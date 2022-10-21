09943d3a09af5494e43f6427cde151f787aceaae
Junyang Xing <1057067557@qq.com>
2022-10-21 16:12:44 +0800

    Adding directory 10.21_class_5 with cmake file.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -39,12 +39,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/xingjunyang/CLionProjects/2022-CPL-Coding/cmake-build-debug/10.7_class_3/Homework/cmake_install.cmake")
+  include("/Users/xingjunyang/CLionProjects/2022-CPL-Coding/cmake-build-debug/10.21_class_5/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/xingjunyang/CLionProjects/2022-CPL-Coding/cmake-build-debug/10.14_class_4/Homework/cmake_install.cmake")
+  include("/Users/xingjunyang/CLionProjects/2022-CPL-Coding/cmake-build-debug/10.21_class_5/Homework/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
